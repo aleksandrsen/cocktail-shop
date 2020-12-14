bf8affcbaf216221da122be850d22ed3904dc809
Aleksandr Oksen <aledor777@gmail.com>
2020-12-14 19:12:58 +0200

    add typescript

--- src/actions/cocktails.tsx
+++ src/actions/cocktails.tsx
@@ -15,23 +15,23 @@ export const fetchCocktailsList = () => ({
   type: FETCH_COCKTAILS_LIST_REQUEST,
 });
 
-export const fetchCocktailsDetails = (id) => ({
+export const fetchCocktailsDetails = (id: string) => ({
   type: FETCH_COCKTAIL_DETAILS_REQUEST,
   id,
 });
 
-export const sendCocktailReview = (id, data) => ({
+export const sendCocktailReview = (id: string, data: {}) => ({
   type: SEND_COCKTAIL_REVIEW_REQUEST,
   id,
   data,
 });
 
-export const setLikeCocktailReview = (id) => ({
+export const setLikeCocktailReview = (id: string) => ({
   type: LIKE_COCKTAIL_REVIEW_REQUEST,
   id,
 });
 
-export const setDislikeCocktailReview = (id) => ({
+export const setDislikeCocktailReview = (id: string) => ({
   type: DISLIKE_COCKTAIL_REVIEW_REQUEST,
   id,
 });
