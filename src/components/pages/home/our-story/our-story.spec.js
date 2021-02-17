d5b304b84a8c1fb27bbd24261c8984d37aa4f855
Aleksandr Oksen <aledor777@gmail.com>
2021-02-17 19:26:36 +0200

    update blog-news-item tests with snapshot

--- src/components/pages/home/our-story/our-story.spec.js
+++ src/components/pages/home/our-story/our-story.spec.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, render } from "enzyme";
 import OurStory from "./our-story";
 
 describe("OurStory component", () => {
   it("should render OurStory component", () => {
-    // expect(1).toBe(1);
     const component = shallow(<OurStory />);
-    const wrapper = component.find(".ourStory");
-    // console.log(component.debug());
-    expect(wrapper.length).toBe(1);
+    expect(component).toMatchSnapshot();
   });
 });
